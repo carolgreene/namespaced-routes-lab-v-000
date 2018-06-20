83813427bc6280e9c6113ec860873ef930e5f4b3
Carol Greene <carolgreene3@gmail.com>
2018-06-20 23:08:44 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,19 +1,10 @@
 class ArtistsController < ApplicationController
 
-
   def index
-   if @preferences && @preferences.artist_sort_order
-     @artists = Artist.order(name: @preferences.artist_sort_order)
-   else
-     @artists = Artist.all
-   end
- end
-
-  #def index
-    #p = Preference.first_or_create(artist_sort_order: "ASC")
+    p = Preference.first_or_create(artist_sort_order: "ASC")
   #  binding.pry
-    #@artists = Artist.order(name: p.artist_sort_order)
-  #end
+    @artists = Artist.order(name: p.artist_sort_order)
+  end
 
   def show
     @artist = Artist.find(params[:id])
